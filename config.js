5cc9efa56b33f7db06fc1843b73b2f1170f56212
smaxwellstewart <smaxwellstewart@gmail.com>
2016-02-13 15:27:12 -0500

    added procfile and config

---
+++ config.js
@@ -0,0 +1,70 @@
+var Confidence = require('confidence');
+var criteria = {
+    env: process.env.NODE_ENV
+};
+
+
+var config = {
+    $meta: 'This file configures the plot device.',
+    projectName: 'faps',
+    port: {
+        web: {
+            $filter: 'env',
+            test: 9000,
+            $default: process.env.PORT
+        }
+    },
+    authAttempts: {
+        forIp: 50,
+        forIpAndUser: 7
+    },
+    mongodb: {
+        $filter: 'env',
+        production: {
+            url: process.env.MONGOLAB_URI,
+            autoIndex: false
+        },
+        test: {
+            url: 'mongodb://heroku_h18bbx6s:4b8loiitmpqnij48ulq9eodp1d@ds061335.mongolab.com:61335/heroku_h18bbx6s-test',
+            autoIndex: true
+        },
+        $default: {
+            url: 'mongodb://heroku_h18bbx6s:4b8loiitmpqnij48ulq9eodp1d@ds061335.mongolab.com:61335/heroku_h18bbx6s',
+            autoIndex: true
+        }
+    },
+    nodemailer: {
+        host: 'gmail.com',
+        port: 8888,
+        secure: true,
+        auth: {
+            user: 'sadasd',
+            pass: 'sadas'
+        }
+    },
+    system: {
+        fromAddress: {
+            name: 'asdasd',
+            address: 'sadasd'
+        },
+        toAddress: {
+            name: 'asdsad',
+            address: 'sadas'
+        }
+    }
+};
+
+
+var store = new Confidence.Store(config);
+
+
+exports.get = function (key) {
+
+    return store.get(key, criteria);
+};
+
+
+exports.meta = function (key) {
+
+    return store.meta(key, criteria);
+};
