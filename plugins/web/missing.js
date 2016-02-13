4142d53c955add72a3eb476aed03c72b0eb470e2
smaxwellstewart <smaxwellstewart@gmail.com>
2016-02-13 15:55:10 -0500

    removed 404 handler

--- plugins/web/missing.js
+++ plugins/web/missing.js
@@ -1,14 +1,14 @@
 exports.register = function (plugin, options, next) {
 
-    plugin.route({
-        method: 'GET',
-        path: '/{path*}',
-        handler: function (request, reply) {
-            return reply.view('404', {
-            	title: 'Page not found.'
-            }).code(404); 
-        }
-    });
+    // plugin.route({
+    //     method: 'GET',
+    //     path: '/{path*}',
+    //     handler: function (request, reply) {
+    //         return reply.view('404', {
+    //         	title: 'Page not found.'
+    //         }).code(404);
+    //     }
+    // });
 
     next();
 };
